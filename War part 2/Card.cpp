b82d91d98a4dea62d58ff07bc350b8d7967701e6
Ala'alddin Al-Migdad <44620254+almigdad1@users.noreply.github.com>
2019-02-15 19:48:08 -0500

    Add files via upload

---
+++ War part 2/Card.cpp
@@ -0,0 +1,44 @@
+//
+// Ala'alddin Al-Migdad
+// ID#2891368
+// aa186@zips
+//
+
+#include "card.h"
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& out, Suit s)
+{
+	switch(s)
+    {
+		case clubs: return out << "Clubs";
+		case diamonds: return out << "Diamonds";
+		case hearts: return out << "Hearts";
+		case spades: return out << "Spades";
+	}
+}
+// sets values to each card
+std::ostream& operator<<(std::ostream& out, Rank r)
+{
+	switch(r)
+	{
+	case ace: return out << "Ace";
+	case two: return out << "2";
+	case three: return out << "3";
+	case four: return out << "4";
+	case five: return out << "5";
+	case six:  return out << "6";
+	case seven: return out << "7";
+	case eight: return out << "8";
+	case nine: return out << "9";
+	case ten: return out << "10";
+	case jack: return out << "Jack";
+	case queen: return out << "Queen";
+	case king: return out << "King";
+	}
+}
+
+std::ostream& operator<<(std::ostream& out, Card c)
+{
+    return out << c.getValue() << " of " << c.getSuit();
+}
---
+++ War part 2/Card.h
@@ -0,0 +1,61 @@
+//
+// Ala'alddin Al-Migdad
+// ID#2891368
+// aa186@zips
+//
+
+#ifndef ___CARD_H
+#define ___CARD_H
+
+#include <string>
+using std::string;
+
+enum Suit
+ {
+     clubs,
+     diamonds,
+     hearts,
+     spades
+ };
+
+enum Rank
+ {
+     two,
+     three,
+     four,
+     five,
+     six,
+     seven,
+     eight,
+     nine,
+     ten,
+     jack,
+     queen,
+     king,
+     ace
+ };
+
+class Card {
+public:
+    Card (Rank r = two, Suit s = clubs) : bits((unsigned)s << 4 | (unsigned)r) { }
+    Rank getValue() const { return(Rank) (0b00001111 & bits); }
+    Suit getSuit() const { return(Suit) ((0b11110000 & bits) >> 4); }
+    bool operator==(Card c) { return bits == c.bits; }
+    bool operator!=(Card c) { return !(bits == c.bits); }
+    friend bool operator<(Card a, Card b) { return a.getValue() < b.getValue(); }
+    friend bool operator>(Card a, Card b) { return b.getValue() < a.getValue(); }
+    friend bool operator<=(Card a, Card b) { return !(b.getValue() < a.getValue()); }
+    friend bool operator>=(Card a, Card b) { return !(a.getValue() < b.getValue()); }
+
+private:
+		Rank rank;
+		Suit suit;
+		unsigned char bits;
+};
+
+//Overloading ostream operators
+std::ostream& operator<<(std::ostream& out, Suit s);
+std::ostream& operator<<(std::ostream& out, Rank r);
+std::ostream& operator<<(std::ostream& out, Card c);
+
+#endif
