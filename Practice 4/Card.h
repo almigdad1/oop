f1f4b4ccc2bd3a75254466875b59cdc966b0a870
Ala'alddin Al-Migdad <44620254+almigdad1@users.noreply.github.com>
2019-03-14 17:13:25 -0400

    Add files via upload

---
+++ Practice 4/Card.h
@@ -0,0 +1,151 @@
+//
+// Ala'alddin Al-Migdad
+// ID#2891368
+// aa186@zips
+//
+
+#ifndef ___CARD_H
+#define ___CARD_H
+#include <assert.h>
+#include <string>
+using std::string;
+
+// LSB
+enum Suit
+ {
+     clubs,
+     diamonds,
+     hearts,
+     spades
+ };
+
+// RSB
+enum Rank
+ {
+     two,
+     three,
+     four,
+     five,
+     six,
+     seven,
+     eight,
+     nine,
+     ten,
+     jack,
+     queen,
+     king,
+     ace
+ };
+
+ enum Color
+{
+    Black,
+    Red
+};
+
+enum Kind
+{
+    StandardKind,
+    JokerKind
+};
+
+class Card {
+public:
+    Card (Rank r = two, Suit s = clubs) : bits((unsigned)s << 4 | (unsigned)r) { }
+    Card(Color c) : bits((unsigned)(1) << 6 | (unsigned)c << 7) { }
+    Rank getValue() const { return(Rank) (0b00001111 & bits); }
+    Suit getSuit() const { return(Suit) ((0b11110000 & bits) >> 4); }
+    friend bool operator==(Card a, Card b) {  return a.getValue() == b.getValue(); }
+    friend bool operator!=(Card a, Card b) { return a.getValue() != b.getValue(); }
+    friend bool operator<(Card a, Card b) { return a.getValue() < b.getValue(); }
+    friend bool operator>(Card a, Card b) { return b.getValue() < a.getValue(); }
+    friend bool operator<=(Card a, Card b) { return !(b.getValue() < a.getValue()); }
+    friend bool operator>=(Card a, Card b) { return !(a.getValue() < b.getValue()); }
+
+    Color getColor() const
+    {
+        if(((bits >> 6) & 0b1) == 1)
+            return (Color) ((bits >> 7) & 0b1);
+        else
+            return (Color) - 1;
+    }
+
+private:
+		Rank rank;
+		Suit suit;
+		unsigned char bits;
+};
+
+class Joker
+{
+    Color color;
+
+public:
+    Joker(Color c) : color(c) { }
+    Color getColor() const { return color; }
+};
+
+union PlayingCardUnion
+{
+    Card standardCard;
+    Joker jokerCard;
+
+    PlayingCardUnion(Rank r, Suit s) : standardCard(r, s) { }
+    PlayingCardUnion(Color c) : jokerCard(c) { }
+};
+
+class PlayingCard
+{
+    Kind kind;
+    PlayingCardUnion playingCardUnion;
+
+public:
+    PlayingCard(Rank r, Suit s) : kind(StandardKind), playingCardUnion(r, s) { }
+    PlayingCard(Color c) : kind(JokerKind), playingCardUnion(c) { }
+    bool isStandardCard() const { return kind == StandardKind; }
+    bool isJokerCard() const { return kind == JokerKind; }
+    //Get function for rank
+    Rank getvalue() const
+    {
+        assert(isStandardCard());
+
+        return playingCardUnion.standardCard.getValue();
+    }
+    //Get function for suit
+    Suit getSuit() const
+    {
+        assert(isStandardCard());
+
+        return playingCardUnion.standardCard.getSuit();
+    }
+    //Get function for colour
+    Color getColor() const
+    {
+        assert(isJokerCard());
+
+        return playingCardUnion.jokerCard.getColor();
+    }
+    //Get function for Standard card
+    Card getStandardCard() const
+    {
+        assert(isStandardCard());
+
+        return playingCardUnion.standardCard;
+    }
+    //Get function for Joker card
+    Joker getJokerCard() const
+    {
+        assert(isJokerCard());
+
+        return playingCardUnion.jokerCard;
+    }
+
+
+};
+
+//Overloading ostream operators
+std::ostream& operator<<(std::ostream& out, Suit s);
+std::ostream& operator<<(std::ostream& out, Rank r);
+std::ostream& operator<<(std::ostream& out, Card c);
+
+#endif
---
+++ Practice 4/War.cpp
@@ -0,0 +1,202 @@
+//
+// Ala'alddin Al-Migdad
+// ID#2891368
+// aa186@zips
+//
+
+#include "War.h"
+#include "Card.h"
+
+// cap the rounds at 5000 so game stops at a point.
+const int War::roundsMax = 5000;
+Hand War::P1;
+Hand War::P2;
+bool War::Skip = false;
+int War::rounds = 1;
+
+War::War() {}
+
+void War::Start()
+{
+    cout << "This is a game of War... At any time in the game you may enter any letter to skip to the end, Enjoy!!" << endl << endl;
+		rounds = 1;
+		Skip = false;
+		//initialize the deck and each players hands
+		Hand deck = Hand::fullDeck();
+		deck.Shuffle();
+		P1 = Hand();
+		P2 = Hand();
+
+		//Deal the cards out to the players
+		//A full deck has an even number of cards
+		while (!deck.empty())
+		{
+			P1.placeBottom(deck.drawTop());
+			P2.placeBottom(deck.drawTop());
+		}
+
+		//Compare the cards until one player runs out of cards
+		while (!P1.empty() && !P2.empty())
+		{
+			battle();
+
+			if (++rounds > roundsMax)
+			{
+				cout << "This game of War has gone on for too long." << endl;
+				break;
+			}
+
+			if (!Skip) checkSkip();
+		}
+
+		// print results
+		winner();
+
+}
+
+
+void War::battle()
+{
+	Card p1Card = P1.drawTop();
+	Card p2Card = P2.drawTop();
+
+	cout << "Player1 " << "[" << P1.size()+1 << "]: " << p1Card << endl << "Player2 " << "[" << P2.size()+1 << "]: " << p2Card << endl;
+
+	if (p1Card == p2Card)
+	{
+		int winner = war();
+
+		if (winner == 1)
+		{
+			P1.placeBottom(p1Card);
+			P1.placeBottom(p2Card);
+
+			cout << endl << "Player1";
+		}
+		else if (winner == 2)
+		{
+			P2.placeBottom(p1Card);
+			P2.placeBottom(p2Card);
+
+			cout << endl << "Player2";
+		}
+		cout << " won the battle!" << endl;
+	}
+	else if (p1Card > p2Card)
+	{
+		P1.placeBottom(p1Card);
+		P1.placeBottom(p2Card);
+		cout << endl << "Player1 won this round." << endl;
+	}
+	else if (p2Card > p1Card)
+	{
+		P2.placeBottom(p1Card);
+		P2.placeBottom(p2Card);
+		cout << endl << "Player2 won this round." << endl;
+	}
+}
+
+int War::war()
+{
+	Hand warCards;
+	Card p1Card, p2Card;
+	int winner = 0;
+
+	do
+	{
+		cout << "Get ready, WAR!!" << endl;
+
+		if (!Skip) checkSkip();
+
+		//If a player is out of cards, then the other player wins
+		if (P1.empty())
+		{
+			cout << "Player1 is out of cards." << endl;
+			winner = 2;
+			break;
+		}
+		else if (P2.empty())
+		{
+			cout << "Player2 is out of cards." << endl;
+			winner = 1;
+			break;
+		}
+
+		//Put down three extra cards for each player
+		for (int i = 0; i < 3; i++)
+		{
+			//Save one card for the comparison
+			if (P1.size() > 1) warCards.placeBottom(P1.drawTop());
+			if (P2.size() > 1) warCards.placeBottom(P2.drawTop());
+		}
+
+		//We'll compare the next two cards
+		p1Card = P1.drawTop();
+		p2Card = P2.drawTop();
+		warCards.placeBottom(p1Card);
+		warCards.placeBottom(p2Card);
+
+        cout << "Player1 " << "[" << P1.size()+1 << "]: " << p1Card << endl << "Player2 " << "[" << P2.size()+1 << "]: " << p2Card << endl;
+
+	} while (p1Card == p2Card);
+
+	//Give the spoils to the winner
+	if (p1Card > p2Card || winner == 1)
+	{
+		int cardSize = warCards.size();
+		for (int i = 0; i < cardSize; i++)
+			P1.placeBottom(warCards.drawTop());
+
+		winner = 1;
+	}
+	else if (p2Card > p1Card || winner == 2)
+	{
+		int cardSize = warCards.size();
+		for (int i = 0; i < cardSize; i++)
+			P2.placeBottom(warCards.drawTop());
+
+		winner = 2;
+	}
+
+	return winner;
+}
+
+void War::winner()
+{
+	//Check if an infinite loop scenario has occurred.
+	if (rounds > roundsMax)
+	{
+		cout << "The war ends in a DRAW." << endl << endl;
+	}
+	else
+	{
+		if (P1.empty())
+		{
+			cout << "Player2 won the war!" << endl << endl;;
+		}
+		else //If player2.empty()
+		{
+			cout << "Player1 won the war!" << endl << endl;;
+		}
+	}
+}
+
+void War::checkSkip()
+{
+	cin.sync();
+	char ans;
+	cin.get(ans);
+	if (isalpha(ans))
+        Skip = true;
+	else
+        Skip = false;
+}
+
+int main()
+{
+    Card test1{Red}, test2{Black};
+    std::cout << test1.getColor() << " " << test2.getColor() << endl;
+	War::Start();
+
+	return 0;
+}
